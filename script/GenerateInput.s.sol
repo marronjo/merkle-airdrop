5d3227aab83c044908437a29c8f906e1461d4afb
Joe Marron <marronjo@tcd.ie>
2025-06-11 16:05:26 +0100

    increase size of merkle tree

--- script/GenerateInput.s.sol
+++ script/GenerateInput.s.sol
@@ -10,7 +10,7 @@ contract GenerateInput is Script {
     uint256 private constant AMOUNT = 25 * 1e18;
     string[] types = new string[](2);
     uint256 count;
-    string[] whitelist = new string[](4);
+    string[] whitelist = new string[](8);
     string private constant  INPUT_PATH = "/script/target/input.json";
 
     function run() public {
@@ -22,6 +22,10 @@ contract GenerateInput is Script {
         whitelist[1] = "0x29E3b139f4393aDda86303fcdAa35F60Bb7092bF";    // "user1"
         whitelist[2] = "0x537C8f3d3E18dF5517a58B3fB9D9143697996802";    // "user2"
         whitelist[3] = "0xc0A55e2205B289a967823662B841Bd67Aa362Aec";    // "user3"
+        whitelist[4] = "0x90561e5Cd8025FA6F52d849e8867C14A77C94BA0";    // "user4"
+        whitelist[5] = "0x22068447936722AcB3481F41eE8a0B7125526D55";    // "user5"
+        whitelist[6] = "0xc1268511E6bC61c44C096f7F25B813Bd5531b64a";    // "user6"
+        whitelist[7] = "0xD2D67024f7cA52d0F49d02329DDFD1b3Edae9351";    // "user7"
         count = whitelist.length;
         string memory input = _createJSON();
         // write to the output file the stringified output json tree dump
